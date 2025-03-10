6cc69d431d8f95957f36b55685b5afb5a925fe4b
Malyadri <malyadrimarthula@gmail.com>
2025-03-10 17:57:14 +0530

    Initial Commit

---
+++ SwiftUI-AnimatedApp/SwiftUI_AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimatedAppApp.swift
+//  SwiftUI-AnimatedApp
+//
+//  Created by Malyadri Reddy on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
